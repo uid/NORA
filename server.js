028eab3ab21994549131cd8a425b16caad72d702
Denzil <denzils@mit.edu>
2013-06-17 23:59:08 -0400

    got hightlighting to work and next sentence stuff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-	var app, everyone, express, http, httpserver, nowjs, io;
+	var app, everyone, express, http, httpserver, httpapp, nowjs;
     
     express = require('express');
 
@@ -8,8 +8,8 @@
 	app = express();
 
 	httpserver = http.createServer(app);
-    
-    httpserver.listen(8080);
+
+	httpserver.listen(8080);
 
 	nowjs = require('now');
 
@@ -19,12 +19,40 @@
 		return app.use(express["static"](__dirname + '/static/'));
 	});
 
-	everyone.now.sendNewChat = function(chatMessage, tag, msg_id) {
-		return everyone.now.gotNewChat(chatMessage, tag, msg_id);
+	everyone.now.sendNewChat = function(chatMessage, tag, msg_id, selectedText) {
+		return everyone.now.gotNewChat(chatMessage, tag, msg_id, sentence, selectedText);
 	};
 
 	everyone.now.serverMoveMsg = function(id, pos) {
 		return everyone.now.moveMsg(id, pos);
 	}
+    
+    var users = 0; 
+    var numReady = 0;
+    var sentence = 0;
+    everyone.on("connect", function(){
+        users+=1;
+        sentence = 0;
+    });
+    
+    everyone.on("disconnect", function(){
+        users -=1;
+    });
+    
+    everyone.now.nextSentenceServer = function() {
+        return everyone.now.nextSentence(users, numReady);
+    }
+    
+    everyone.now.incrementUsersReady = function() {
+        numReady +=1;
+    }
 
+    everyone.now.updateServer = function() {
+        numReady = 0;
+        return everyone.now.updateSentence(sentence);
+    }
+    
+    everyone.now.updateSentenceNumServer = function(sentenceNum) {
+        sentence = sentenceNum;
+    }
 }).call(this);
--- static/client.js
+++ static/client.js
@@ -1,13 +1,74 @@
-var socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
-
-now.ready(function () {
-    $('#nextSentence').click(function (event) {
-		prompt("I was clicked", "yes you were");
+now.ready(function () {    
+    $('#selectedText').click(function (event) {
+        var selectedText=(
+            window.getSelection
+            ?
+            window.getSelection()
+            :
+            document.getSelection
+            ?
+                document.getSelection()
+            :
+                document.selection.createRange().text
+        );
+        var tag = "";
+        if ($('#grammar').is(':checked')) tag = tag.concat(" G ");
+        if ($('#logic').is(':checked')) tag = tag.concat(" L ");
+        if ($('#style').is(':checked')) tag = tag.concat(" S ");
+        var d = new Date();
+        var msg_id = (d.getMonth()+d.getFullYear()+d.getTime()).toString()
+        
+        now.sendNewChat($('#chatbox').val(), tag, msg_id, String(selectedText));
+        $('#chatbox').val('');
+        tag = "";
+        $('#grammar').attr('checked', false);
+        $('#logic').attr('checked', false);
+        $('#style').attr('checked', false);
 	});
+    
+    var sentences = $('#content').text().split(".");   
+    
+    $('#content').empty();
+    
+    for (var s in sentences) {
+        var text = '<span class=grey id=sent_'+s+'>' + sentences[s] + '.</span>';
+        if (s == sentences.length - 1) break;
+        $('#content').append(text);
+    }
+    
+    $('#sent_0').removeClass('grey');
+    
+    $('#nextSentence').click(function (event) {
+        now.incrementUsersReady();
+		now.nextSentenceServer();
+	});
+    
+    now.nextSentence = function(users, numReady) {
+        $('div#usersReady').text("Users Ready:");
+        $('div#pplReady').text(numReady+"/"+users);
+        if(numReady >= users) {
+            $('div#pplReady').text("0/"+users);
+            now.updateServer();
+        }
+    }
+    
+    now.updateSentence = function(sentenceNum) {
+        sentenceNum+=1;
+        now.updateSentenceNumServer(sentenceNum);
+        $('#sent_'+sentenceNum).removeClass('grey');
+        $('#sent_'+(sentenceNum-1)).addClass('grey');
+        document.getElementById("nextSentence").disabled= false;
+        console.log(sentenceNum);
+        console.log(sentences.length - 1);
+        if (sentenceNum == sentences.length - 1) {
+            for (var s in sentences) {
+                $('#sent_'+s).removeClass('grey');
+            }
+            document.getElementById("nextSentence").disabled= true;
+            $('div#usersReady').text("");
+            $('div#pplReady').text("");
+        }
+    };
     
     $('#chatbox').keypress(function (event) {
 		if (event.keyCode === 13) { // enter
@@ -17,7 +78,7 @@ now.ready(function () {
 			if ($('#style').is(':checked')) tag = tag.concat(" S ");
             var d = new Date();
             var msg_id = (d.getMonth()+d.getFullYear()+d.getTime()).toString()
-			now.sendNewChat($('#chatbox').val(), tag, msg_id);
+			now.sendNewChat($('#chatbox').val(), tag, msg_id, "");
 			$('#chatbox').val('');
 			tag = "";
 			$('#grammar').attr('checked', false);
@@ -26,20 +87,46 @@ now.ready(function () {
 			return false;
 		}
 	});
-
+    
 	now.moveMsg = function(id, pos) {
 		$(id).css('top', pos.top+'px'); 
 		$(id).css('left', pos.left+'px');
 	};
+    
+    now.selecting = function(texty, msgID){
+        var spanExists = document.getElementById('st_'+msgID);
+        if (spanExists==null) {
+            var searching = unescape(texty);
+            searching = searching.substring(1,searching.length-1);
+            var para = String(document.getElementById('content').innerHTML);
+            var start = para.indexOf(searching);
+            var textSpan = '<span class=selectedText id=st_'+msgID+'>' + para.substring(start, (start+searching.length)) + '</span>';
+            var newpara = para.substring(0,start)+ textSpan+ para.substring((start+searching.length), para.length);
+            document.getElementById('content').innerHTML = newpara;
+            document.getElementById('st_'+msgID);
+        }
+        else {
+            if($('#st_'+msgID).hasClass('selectedText')) {  
+                $('#st_'+msgID).removeClass('selectedText');
+            }
+            else {
+                $('#st_'+msgID).addClass('selectedText');
 
-	now.gotNewChat = function(chatMessage, tag, msg_id) {
-		$('#messages').append('<div class=msg id=msg_'+msg_id+' style="position:absolute; display:inline-block; width:15%; word-wrap: break-word; border:1px solid #999;background-color:#FFFF99;"> (sentence) ' +msg_id + ' '+ tag + '<br>' + chatMessage + '</div>');
-	    
+            }
+        }
+    }
+    
+	now.gotNewChat = function(chatMessage, tag, msg_id, sentence,selectedText) {
+        var texty = escape(String(selectedText));
+        $('#messages').append('<div class=msg id=msg_'+msg_id+' onclick=now.selecting(&apos;"'+texty+'"&apos;,'+msg_id+')>('+(sentence+1)+ ') '+ tag + '<br>' + chatMessage + '</div>');
+        /*  var text = '<span class=selectedText id=st_'+msg_id+'>' + selectedText + '.</span>';   
+        $('#messages').append('<div class=msg id=msg_'+msg_id+' onclick=now.selecting('+msg_id+')>'+(sentence+1)+ ' '+ tag + '<br>' + chatMessage + '</div>');*/
+         
 	    var ypos = 10;
 	    var xpos = (parseInt(msg_id)%60)*10;
 	    $('#msg_'+msg_id).css('top', ypos+'px');
 	    $('#msg_'+msg_id).css('left', xpos+'px');
-		
+        
 		$(function() {
 			$('#msg_'+msg_id).draggable({
 				stop: function(event, ui) {
@@ -61,4 +148,6 @@ now.ready(function () {
 	   });
 	 
 	};
-});
+    
+    
+}); 
